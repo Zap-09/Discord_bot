0cdaad971f81be68d86e0e0333fa868308958826
Zap <177562262+Zap-09@users.noreply.github.com>
2026-01-20 21:16:55 +0600

    fixed delete_message_with_delay

--- utils/global_utils.py
+++ utils/global_utils.py
@@ -1,7 +1,11 @@
 import asyncio
+import discord
 
-async def delete_message_with_delay(ctx,delete_delay:int = 5):
-    """ Deletes a message with a delay. 'delete_delay' is the delay time in mins """
-    delete_delay = delete_delay * 60
+async def delete_message_with_delay(message, delete_delay: int = 5):
+    delete_delay *= 60
     await asyncio.sleep(delete_delay)
-    await ctx.delete()
+
+    try:
+        await message.delete()
+    except (discord.NotFound, discord.Forbidden):
+        pass
